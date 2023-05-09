91379fe90e1a930915148eec7c5216b865372886
Ondřej Kašpar <kaspi@kaspi.dev>
2023-05-09 10:23:10 +0200

    feat: updated App, CSS baseline, router
    Using lazy SignOut component for codesplitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,20 @@
-import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
-import "./App.css"
+import { lazy } from "react"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import CssBaseline from "@mui/material/CssBaseline"
+
+import { SIGN_OUT } from "./constants/routes"
+
+const SignOut = lazy(() => import("./components/SignOut"))
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path={SIGN_OUT} element={<SignOut />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
